2cf638a784b94ca78ed833891638fc0974530601
Ana Beatriz <moreiraanabe394@gmail.com>
2025-09-15 11:00:53 -0300

    Desafio concluido

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,15 @@
 namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+    public class Iphone : Smartphone
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+        //Construtor da base tem parâmetros obrigatórios. Smartphone não tem um construtor vazio (public Smartphone() {}). Ele exige os valores numero, modelo, imei, memoria. Portanto, o compilador obriga você a repassar esses valores usando : base(...).
+        public Iphone(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
+        {
+        }
+
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando {nomeApp} no Iphone...");
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+
+// Criando Nokia com todos os dados necessários
+Nokia k = new Nokia("96738-3749", "Nokia Tijolão", "987654321", 64);
+
+k.Ligar();
+k.ReceberLigacao();
+k.InstalarAplicativo("Telegram");
+
+// Criando iPhone com todos os dados necessários
+Iphone i = new Iphone("98459-5294", "iPhone 14", "123456789", 128);
+
+i.Ligar();
+i.ReceberLigacao();
+i.InstalarAplicativo("WhatsApp");
+
